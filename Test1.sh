aeda73cb0853326b1419fbd587ec228c3d9fe368
zeus <33592162+1nam@users.noreply.github.com>
2020-08-29 19:14:37 -0400

    Create Test1.sh

---
+++ Test1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# using hash tag makes text hidden only the scripter can see!!!
+# have fun make your own bash script from this script.
+echo
+echo
+echo
+echo
+echo write multiple lines on one line open and read script.
+echo
+echo
+echo -e "This is your First Line \nThis is your Second Line"
+echo
+echo # these empty echo's space out a section.
+echo
+echo
+echo how read echo and sleep actually work. plus whoami command
+echo
+echo
+echo write three favorite colors and press enter.
+read color1 color2 color3
+echo Your first color was: $color1 really pretty color.
+echo Your second color was: $color2 one of my favorites as well.
+echo Your third color was: $color3  awesome color choice.
+echo The terminal will auto close in 17 seconds. 
+echo This script will terminate in 10 seconds.
+sleep 1
+echo 1.
+sleep 1
+echo 2..
+sleep 1
+echo 3...
+sleep 1
+echo 4....
+sleep 1
+echo 5.....
+sleep 1
+echo 6......
+sleep 1
+echo 7.......
+sleep 1
+echo 8........
+sleep 1
+echo 9.........
+echo 10 Good Bye.
+whoami
+sleep 7
